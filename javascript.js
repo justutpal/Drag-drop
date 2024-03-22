23836128f4a24efd0b72e86459d3caea8402abb5
justutpal <kumarutpal2112001@gmail.com>
2024-03-22 22:06:53 +0530

    Resolved bug

--- javascript.js
+++ javascript.js
@@ -3,25 +3,30 @@ let right = document.querySelector(".right");
 let items = document.querySelectorAll(".items");
 
 // function for drag
+let selected = null
+
 
 for (const item of items) {
-    item.addEventListener('dragstart', (e) => {
+    item.addEventListener('dragstart', () => {
         
-        let selected = e.target
+        selected = item
+    })
+}
 
         right.addEventListener('dragover', (e) => {
             e.preventDefault();
         })
+
         right.addEventListener('drop', (e) => {
             right.appendChild(selected);
             selected = null
         })
+
         left.addEventListener('dragover', (e) => {
             e.preventDefault()
         })
+        
         left.addEventListener('drop', (e) => {
             left.appendChild(selected)
             selected = null;
         })
-    })
-}
